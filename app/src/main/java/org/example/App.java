0b952760a01caed12c58691029ab0ca6b80e077c
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-03-17 20:45:33 -0500

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,49 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Your code goes here!");
+
+    FuzzyListGenerator generator = new FuzzyListGenerator();
+
+    
+    ArrayList<Fuzzy> randomList = generator.randomizedRainbowFuzzies();
+    int linearIndex = linearSearchGold(randomList);
+    System.out.println("Linear Search (random list): " + linearIndex);
+
+   
+    ArrayList<Fuzzy> sortedList = generator.sortedRainbowFuzzies();
+    int binaryIndex = binarySearchGold(sortedList);
+    System.out.println("Binary Search (sorted list): " + binaryIndex);
+  }
+
+  
+  public static int linearSearchGold(ArrayList<Fuzzy> fuzzies) {
+    for (int i = 0; i < fuzzies.size(); i++) {
+      if (fuzzies.get(i).color.equals("gold")) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static int binarySearchGold(ArrayList<Fuzzy> fuzzies) {
+    int left = 0;
+    int right = fuzzies.size() - 1;
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      String color = fuzzies.get(mid).color;
+
+      if (color.equals("gold")) {
+        return mid;
+      } else if (color.compareTo("gold") < 0) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
   }
 }
